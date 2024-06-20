2f9824fe78e88221b9522b475dc485bacf607e14
overlrd <premieraugust28@gmail.com>
2024-06-20 13:18:49 +0000

    edit tmux/zsh config
    fix zsh completion not working
    added basic tmux config

--- .config/scripts/zsh/completion.zsh
+++ .config/scripts/zsh/completion.zsh
@@ -1,23 +1,6 @@
 # Should be called before compinit
 zmodload zsh/complist
 
-export HISTFILE="$ZDOTDIR/.zhistory"    # History filepath
-export HISTSIZE=10000                   # Maximum events for internal history
-export SAVEHIST=10000                   # Maximum events in history file
-
-# Use hjlk in menu selection (during completion)
-# Doesn't work well with interactive mode
-bindkey -M menuselect 'h' vi-backward-char
-bindkey -M menuselect 'k' vi-up-line-or-history
-bindkey -M menuselect 'j' vi-down-line-or-history
-bindkey -M menuselect 'l' vi-forward-char
-
-bindkey -M menuselect '^xg' clear-screen
-bindkey -M menuselect '^xi' vi-insert                      # Insert
-bindkey -M menuselect '^xh' accept-and-hold                # Hold
-bindkey -M menuselect '^xn' accept-and-infer-next-history  # Next
-bindkey -M menuselect '^xu' undo                           # Undo
-
 autoload -U compinit; compinit
 _comp_options+=(globdots) # With hidden files
 
--- .zshrc
+++ .zshrc
@@ -34,6 +34,7 @@ fi
 source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source ~/.zsh/powerlevel10k/powerlevel10k.zsh-theme
 source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+source ~/.config/scripts/zsh/completion.zsh
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
