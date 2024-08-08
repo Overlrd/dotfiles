53742e733cc1c6e1020b9ec746ccd91ca2af2ee8
overlrd <premieraugust28@gmail.com>
2024-08-08 23:25:57 +0000

    add nord colorscheme

--- .config/nvim/lua/plugins/formatter.lua
+++ .config/nvim/lua/plugins/formatter.lua
@@ -18,7 +18,7 @@ return { -- Autoformat
 			-- Disable "format_on_save lsp_fallback" for languages that don't
 			-- have a well standardized coding style. You can add additional
 			-- languages here or re-enable it for the disabled ones.
-			local disable_filetypes = { c = true, cpp = true }
+			local disable_filetypes = {}
 			return {
 				timeout_ms = 500,
 				lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
@@ -28,9 +28,12 @@ return { -- Autoformat
 			lua = { "stylua" },
 			-- Conform can also run multiple formatters sequentially
 			-- python = { "isort", "black" },
-			--
-			-- You can use 'stop_after_first' to run the first available formatter from the list
 			-- javascript = { "prettierd", "prettier", stop_after_first = true },
+		},
+		filetype = {
+			c = { exe = "clang-format", args = {
+				"-style=file:~/.clang-format",
+			} },
 		},
 	},
 }
