c60f7bf671ab3d983dab8a0e10c1f2c4c1d3713b
Overlrd <premieraugust29@gmail.com>
2024-09-05 08:39:34 +0000

    edit bookmark_browser_tab.sh

--- .config/scripts/bookmark_brower_tab.sh
+++ .config/scripts/bookmark_brower_tab.sh
@@ -1,21 +1,42 @@
 #!/bin/sh
 
-# Find the window ID of the currently focused Brave window
+BROWSER="firefox" 
+BOOKMARKS="$HOME/.bookmarks" 
+
+# Check if the necessary command-line tools are available
+for cmd in xdotool xclip notify-send; do
+    if ! command -v "$cmd" > /dev/null; then
+        echo "Error: Required command '$cmd' is not installed." >&2
+        exit 1
+    fi
+done
+
+# Find the window ID of the currently focused browser window
 window_id=$(xdotool search --onlyvisible --class "$BROWSER" | head -n 1)
 
 if [ -n "$window_id" ]; then
    # Get the window name
     window_name=$(xdotool getwindowname "$window_id")
 
-    # get the url of the focused tab
+    if [ -z "$window_name" ]; then
+        notify-send "Oops!" "Failed to get the window name."
+        exit 1
+    fi
+
+    # activate the window and  get the url of the focused tab
     xdotool windowactivate "$window_id" key --clearmodifiers ctrl+l ctrl+c Escape
     tab_url="$(xclip -o -selection clipboard)"
+
+    if [ -z "$tab_url" ]; then
+        notify-send "Oops!" "Failed to get the tab URL."
+        exit 1
+    fi
 else
     notify-send "Oops!" "No $BROWSER window is active"
     exit 1
 fi
 
-# check if bookmarks file exist
+# check if bookmarks file exists
 if [ ! -e "$BOOKMARKS" ]; then
     > "$BOOKMARKS"
 fi
@@ -23,10 +44,9 @@ fi
 bookmark="$tab_url | $window_name" 
 
 if grep -q "^$bookmark$" $BOOKMARKS ; then
-    notify-send "Oops!" "$window_name already bookmarked"
+    notify-send "Oops!" "$window_name is already bookmarked"
     exit 1;
 else
     echo "$bookmark" >> $BOOKMARKS
+    notify-send "Added!" "$window_name has been bookmarked!"
 fi
-
-notify-send "Added!" "$window_name Bookmarked!"
