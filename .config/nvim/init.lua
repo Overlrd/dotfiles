5decb55f8c4fd63c12bbaa88763c4ca2e0d1c704
augustin <augustin@debian>
2024-10-24 21:40:23 +0000

    edited stuff

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,11 +1,74 @@
-require("config.options")
-require("config.keymaps")
-require("config.lazy")
+-- Options
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.opt.cursorline = true
+vim.opt.guicursor = "n-v-c-i:block"
+vim.opt.number = true
+vim.opt.wrap = false
+vim.opt.clipboard = "unnamedplus"
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.swapfile = false
+vim.opt.undodir = os.getenv("HOME") .. "/.cache/vim/undodir"
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.inccommand = "split"
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.signcolumn = "yes"
+
+-- transparent background
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.cmd([[ :highlight SignColumn guibg=NONE ]])
+vim.cmd("colorscheme retrobox")
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		os.exit(1)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Plugins
 require("lazy").setup({
 	spec = {
-		 { import = "plugins" },
+		{ import = "plugins" },
 	},
 	install = { colorscheme = { "retrobox" } },
 	checker = { enabled = false },
+})
+
+-- Keymaps
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+vim.keymap.set("n", "<leader>f", require("conform").format, {})
+
+vim.keymap.set("n", "<leader>e", require("fzf-lua").files, { desc = "Fzf Files" })
+vim.keymap.set("n", "<leader><leader>", require("fzf-lua").buffers, { desc = "Fzf BUffers" })
+vim.keymap.set("n", "<leader>fg", require("fzf-lua").live_grep, { desc = "live grep" })
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
